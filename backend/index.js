58ef01f0a030c3f88a8e0dab694b7c97d1143431
Anubhav Sahu <anubhavsahu95@gmail.com>
2024-07-20 00:25:29 +0530

    imgurl-local4000

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://e-commerce-backend-xyu4.onrender.com/images/${req.file.filename}`,
+    image_url: `http://localhost:4000/images/${req.file.filename}`,
   });
 });
 

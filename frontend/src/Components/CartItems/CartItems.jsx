93d306bdd777774cadc02e4eebcdc9507bde32a2
Anubhav Sahu <anubhavsahu95@gmail.com>
2024-07-19 19:26:01 +0530

    payment-url-uppdated

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -9,9 +9,9 @@ const CartItems = () => {
 
   const checkoutHandler = async (amount) => {
 
-    const { data: { key } } = await axios.get("http://www.localhost:5000/api/getkey")
+    const { data: { key } } = await axios.get("https://e-commerce-payment.onrender.com/api/getkey")
 
-    const { data: { order } } = await axios.post("http://localhost:5000/api/checkout", {
+    const { data: { order } } = await axios.post("https://e-commerce-payment.onrender.com/api/checkout", {
         amount
     })
 
@@ -23,7 +23,7 @@ const CartItems = () => {
         description: "Tutorial of RazorPay",
         image: "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
         order_id: order.id,
-        callback_url: "http://localhost:5000/api/paymentverification",
+        callback_url: "https://e-commerce-payment.onrender.com/api/paymentverification",
         prefill: {
             name: "Anubhav Sahu",
             email: "anubhav@example.com",

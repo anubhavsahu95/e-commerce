c5f91615243396e1f58a7f76374802cf39a5da56
Anubhav Sahu <anubhavsahu95@gmail.com>
2024-09-10 16:29:58 +0530

    updated allowedHeaders with auth-token

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization','auth-token'],
   credentials: true,
 };
 

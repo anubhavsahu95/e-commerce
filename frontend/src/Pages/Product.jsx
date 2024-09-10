95a69d480858af33433603295c17c5f9d35f2fec
Anubhav Sahu <anubhavsahu95@gmail.com>
2024-09-10 22:41:05 +0530

    loading page added

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -6,13 +6,21 @@ import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 import all_product from '../Components/Assets/all_product'
+import './CSS/Product.css'
 
 const Product = () => {
   const {all_product_online}=useContext(ShopContext);
   const {productId}=useParams();
   // let product=all_product.find((e)=> e.id===Number(productId))
   // if(!product)product=all_product_online.find((e)=> e.id===Number(productId))
-  let product=all_product_online.find((e)=> e.id===Number(productId))
+  let product=all_product_online.find((e)=> e.id===Number(productId));
+  if (!product) {
+    return <div>
+
+     <div className='loading'></div>
+
+     </div>
+  }
   return (
     <div>
       <Breadcrum product={product}/>
